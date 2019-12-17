8d3ecdc30a1d7a7dd15fd93ef6a7087db410e0a0
fedkendi <fedkendi@gmail.com>
2019-12-17 11:54:23 -0300

    Salvando tudo, última aula API

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('./config/config');
 
-const url = 'mongodb+srv://fed_admin:efIo6FVcHp3jjoVN@clusterapi-1edsq.mongodb.net/test?retryWrites=true&w=majority';
+const url = config.bd_string;
 const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
 
 mongoose.connect(url, options);
